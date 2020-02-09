5e1546e43777ea782589140c3488707baf0db810
Laurent <laurent.andrieu19@gmail.com>
2020-02-09 15:51:54 +0100

    Version 3 du Programme de scan

--- Netscan.py
+++ Netscan.py
@@ -3,29 +3,34 @@
 
 import re
 import subprocess
+import scapy.all as scapy
 
 
 def get_config():
-    config = [[], []]
+
     int_cmd = subprocess.check_output('ip link show', shell=True)
     interfaces = re.findall('\d+: (\w+)', str(int_cmd))
-    int_list = []
-    keys = ['interface', 'mac', 'ipv4', 'ipv6', 'netmask', 'broadcast']
-    for i, interface in enumerate(interfaces):
-        cfg_cmd = subprocess.check_output(f'ifconfig {interface}', shell=True)
+    interface = []
+
+    for i, interf in enumerate(interfaces):
+        cfg_cmd = subprocess.check_output(f'ifconfig {interf}', shell=True)
         cfg_cmd = str(cfg_cmd)
         mac = re.findall('ether ([\w{2}:]{17})', cfg_cmd)
         ipv6 = re.findall('inet6 ([\w+:]+)', cfg_cmd)
         ipv4 = re.findall('inet ([\d{3}.+]+)', cfg_cmd)
         broadcast = re.findall('broadcast ([\d{1,3}.]+)', cfg_cmd)
         netmask = re.findall('netmask ([\d{1,3}.]+)', cfg_cmd)
+
         if (mac, ipv4, ipv6, broadcast, netmask):
-            int_list.append({'interface': interface, 'mac': mac, 'ipv4': ipv4, 'ipv6': ipv6, 'netmask': netmask,
+            interface.append({'interf': interf, 'mac': mac, 'ipv4': ipv4, 'ipv6': ipv6, 'netmask': netmask,
                               'broadcast': broadcast})
-        for j in range(len(keys)):
-            if int_list[i][f'{keys[j]}']:
-                config[i].append(int_list[i][f'{keys[j]}'])
-    return config
+    return interface
 
 
-print(get_config())
+def netscan(interfaces):
+    for i in interfaces:
+        for j in i:
+            print(i[f'{j}'])
+
+
+netscan(get_config())
